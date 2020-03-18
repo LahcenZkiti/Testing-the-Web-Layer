7ac237ce7a03d171932019570183c1ba4ceb0752
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-03-18 14:08:46 +0100

    first commit

---
+++ src/test/java/com/example/testing/web/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
